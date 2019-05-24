797b105dd7c4ed663261a4bfad6525d88305c8d0
David <junjian.chen@foxmail.com>
2019-05-24 10:20:42 +0800

    优化画笔橡皮切换

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ var canvas = document.getElementById('canvas')
 var context = canvas.getContext('2d')
 
 autoSetCanvasSize()
-listenerMouse()
+
+/*特性检测*/
+if('ontouchstart' in document.body){
+  listenerTouch()
+}else{
+  listenerMouse()
+}
+
 
 var eraserEnabled = false
 var eraser = document.querySelector('#eraser')
@@ -30,25 +37,31 @@ function autoSetCanvasSize(){
 }
 
 
-function drawCircle(x, y){
+function draw(lastX, lastY, x, y, color,width){
+  if(!color){color="black"}
+  if(!width){width=2}
   context.beginPath()
-  context.fillStyle = 'black'
-  context.arc(x,y,2,0,2*Math.PI)
+  context.fillStyle = color
+  context.arc(x,y,width,0,2*Math.PI)
   context.fill()
   context.closePath()
 
-}
-
-function drawLine(lastX, lastY, nextX, nextY){
   context.beginPath()
-  context.strokeStyle = 'black'
+  context.strokeStyle = color
   context.moveTo(lastX, lastY)
-  context.lineWidth = 5
-  context.lineTo(nextX, nextY)
+  context.lineWidth = width*2
+  context.lineTo(x, y)
   context.stroke()
   context.closePath()
 }
 
+function erase(lastX, lastY, x, y,width){
+  if(!width){width=10}
+  draw(lastX, lastY, x, y, 'white',width)
+}
+
+
+/*绑定鼠标事件：按下鼠标，移动鼠标，松开鼠标*/
 function listenerMouse(){
   
   var using = false
@@ -59,11 +72,6 @@ function listenerMouse(){
     var x = e.clientX
     var y = e.clientY
     lastPoint = {x:x, y:y}
-    if(eraserEnabled){
-      context.clearRect(x-5,y-5,10,10)
-    }else{
-      drawCircle(x,y)
-    }
   }
   
   canvas.onmousemove = function(e){
@@ -72,16 +80,50 @@ function listenerMouse(){
     var newPoint = {x:x, y:y}
     if(using){
       if(eraserEnabled){
-        context.clearRect(x-5,y-5,10,10)
+        erase(lastPoint.x, lastPoint.y, x, y)
+        lastPoint = newPoint
       }else{
-        drawCircle(x,y)
-        drawLine(lastPoint.x,lastPoint.y,newPoint.x,newPoint.y)
+        draw(lastPoint.x,lastPoint.y, x, y)
         lastPoint = newPoint
       }
     }
   }
   
   canvas.onmouseup = function(e){
+    using = false
+    lastPoint = {x: undefined, y: undefined}
+  }
+}
+
+/*绑定触摸事件：按下手指，移动手指，松开手指*/
+function listenerTouch(){
+  
+  var using = false
+  var lastPoint = {x: undefined, y: undefined}
+
+  canvas.ontouchstart = function(e){
+    using = true
+    var x = e.touches[0].clientX
+    var y = e.touches[0].clientY
+    lastPoint = {x:x, y:y}
+  }
+  
+  canvas.ontouchmove = function(e){
+    var x = e.touches[0].clientX
+    var y = e.touches[0].clientY
+    var newPoint = {x:x, y:y}
+    if(using){
+      if(eraserEnabled){
+        erase(lastPoint.x, lastPoint.y, x, y)
+        lastPoint = newPoint
+      }else{
+        draw(lastPoint.x,lastPoint.y,x,y)
+        lastPoint = newPoint
+      }
+    }
+  }
+  
+  canvas.ontouchend = function(e){
     using = false
     lastPoint = {x: undefined, y: undefined}
   }
